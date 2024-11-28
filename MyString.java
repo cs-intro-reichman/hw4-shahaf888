840165333bad966664b149780b54653d977aa205
shahaf888 <92173491+shahaf888@users.noreply.github.com>
2024-11-28 23:17:09 +0200

    fourth

--- MyString.java
+++ MyString.java
@@ -18,13 +18,58 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        if (str.length() == 0){
+            return "";
+        }
+        String helper = "";
+        for (int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+
+            if (c >= 'A' && c <= 'Z'){
+                c = (char) (c + 32);
+                helper = helper + c;
+            }
+            else{
+                helper = helper + c;
+            }
+
+        }
+        return helper;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
-        return false;
+        if (str2.length() > str1.length()){
+            return false;
+        }
+        else if (str2.length() == 0){
+            return true;
+        }
+        else{
+            int i = 0;
+            while (i < str1.length()){
+                if (str2.length() > str1.length() - i){
+                    return false;
+                }
+                if(str1.charAt(i) == str2.charAt(0)){
+                    int j = 0;
+                    int m = i;
+                    while (j < str2.length()){
+                        if(str1.charAt(m) != str2.charAt(j)){
+                            break;
+                        }
+                        else{
+                            m++;
+                            j++;
+                        }
+                    }
+                    if (j == str2.length()){
+                        return true;
+                    }
+                }
+                i++;
+            }
+        }
+        return true;
     }
 }
